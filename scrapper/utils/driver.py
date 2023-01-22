27bdf3381caa57187e9585b09102888c8d0c6308
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2023-01-22 17:13:01 -0500

    feat: runner chrome

--- scrapper/utils/driver.py
+++ scrapper/utils/driver.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import chromedriver_autoinstaller
 
 
 def get_chrome_driver() -> webdriver:
-    chromedriver_autoinstaller.install()
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=[
         '--headless',
         '--disable-gpu',
