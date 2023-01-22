f9c0bc721e89d6f236dfc3414e12143a4cd0ead8
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2023-01-22 17:22:16 -0500

    feat: pyvirtualdisplay

--- scrapper/utils/driver.py
+++ scrapper/utils/driver.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from pyvirtualdisplay import Display
 
 
 def get_chrome_driver() -> webdriver:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=[
-        '--headless',
-        '--disable-gpu',
-        '--no-sandbox',
-        '--disable-dev-shm-usage',
-        "--disable-setuid-sandbox",
-    ])
+    display = Display(visible=0, size=(800, 800))
+    display.start()
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return driver
