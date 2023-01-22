fca05864d37df74faa4ea8a314f8e2c04f760396
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2023-01-22 16:58:39 -0500

    fix: add installer chrome to driver

--- scrapper/utils/driver.py
+++ scrapper/utils/driver.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 
 
 def get_chrome_driver() -> webdriver:
+    chromedriver_autoinstaller.install() 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return driver
