954d7a4d55bad8fd970be430b3a008cc824c995d
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2023-01-22 23:17:18 -0500

    fix: report config

--- scrapper/utils/driver.py
+++ scrapper/utils/driver.py
@@ -1,12 +1,14 @@
+from os import environ
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from pyvirtualdisplay import Display
 
 
 def get_chrome_driver() -> webdriver:
-    display = Display(visible=0, size=(800, 800))
-    display.start()
+    if environ.get("CI") == "true":
+        from pyvirtualdisplay import Display
+        display = Display(visible=0, size=(800, 800))
+        display.start()
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return driver
